10c450b1897aab610c6915aad94910eb242cb9b8
Jeff Smith <jeffmsmithdev@gmail.com>
2016-09-18 19:23:12 -0400

    moving on to adv templates

--- headlines.py
+++ headlines.py
@@ -1,4 +1,5 @@
 import feedparser
+from flask import render_template
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,31 +11,13 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'ars': 'http://feeds.arstechnica.com/arstechnica/index' }
 
 @app.route("/")
-# @app.route("/bbc")
-# def bbc():
-#     return get_news('bbc')
-#
-# @app.route("/cnn")
-# def cnn():
-#     return get_news('cnn')
-#
-# @app.route("/ars")
-# def ars():
-#     return get_news('ars')
-
 @app.route("/<publication>")
 
 def get_news(publication="bbc"):
   feed = feedparser.parse(RSS_FEEDS[publication])
   first_article = feed['entries'][0]
-  return """<html>
-    <body>
-        <h1>Headlines </h1>
-        <b>{0}</b> </ br>
-        <i>{1}</i> </ br>
-        <p>{2}</p> </ br>
-    </body>
-</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+  # return render_template("home.html")
+  return render_template("home.html", title=first_article.get("title"), published=first_article.get("published"), summary=first_article.get("summary"))
 
 
 if __name__ == "__main__":
