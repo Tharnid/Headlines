bbf80a71d94fb619e61c0a7f96c745d2c50a6257
Jeff Smith <jeffmsmithdev@gmail.com>
2016-09-18 17:13:40 -0400

    multi-feeds working

--- headlines.py
+++ headlines.py
@@ -1,42 +1,3 @@
-# import feedparser
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-# # BB = "http://feeds.feedburner.com/breitbart"
-# # AT = "http://feeds.arstechnica.com/arstechnica/index"
-# # EF = "http://feeds.soundcloud.com/users/soundcloud:users:24638646/sounds.rss"
-#
-# RSS_FEEDS = {'bb': "http://feeds.feedburner.com/breitbart",
-#             'at': "",
-#             'ef': "http://feeds.soundcloud.com/users/soundcloud:users:24638646/sounds.rss"}
-#
-# @app.route("/")
-# @app.route("/bb")
-# def bb():
-#     return get_news('bb')
-# @app.route("/at")
-# def at():
-#     return get_news('at')
-#
-#
-# def get_news(publication):
-#     # return "No news is good news"
-#     feed = feedparser.parse(RSS_FEEDS[publication])
-#     first_article = feed['entries'][0]
-#     return """<html>
-#         <body>
-#             <h1>Headlines</h1>
-#             <b>{0}</b><br/>
-#             <i>{1}</i><br/>
-#             <p>{2}</p><br/>
-#         </body>
-#     </html>""".format(first_article.get("title"), first_article.get("published"),
-#         first_article.get("summary"))
-#
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, debug=True)
-
 import feedparser
 from flask import Flask
 
@@ -49,19 +10,21 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'ars': 'http://feeds.arstechnica.com/arstechnica/index' }
 
 @app.route("/")
-@app.route("/bbc")
-def bbc():
-    return get_news('bbc')
+# @app.route("/bbc")
+# def bbc():
+#     return get_news('bbc')
+#
+# @app.route("/cnn")
+# def cnn():
+#     return get_news('cnn')
+#
+# @app.route("/ars")
+# def ars():
+#     return get_news('ars')
 
-@app.route("/cnn")
-def cnn():
-    return get_news('cnn')
+@app.route("/<publication>")
 
-@app.route("/ars")
-def ars():
-    return get_news('ars')
-
-def get_news(publication):
+def get_news(publication="bbc"):
   feed = feedparser.parse(RSS_FEEDS[publication])
   first_article = feed['entries'][0]
   return """<html>
